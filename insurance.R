f5c7731c9fe3dd3b2a03ceb6be31450bd4a8782a
Vijay Mudivedu <Vijay.Mudivedu@iiitb.net>
2018-07-20 10:38:49 +0530

    inital comment

---
+++ insurance.R
@@ -0,0 +1,14 @@
+library(caret)
+library(tidyverse)
+
+
+train <-  read.csv(file = "train.csv",stringsAsFactors = FALSE)
+head(train)
+
+str(train)
+
+#check for NAs
+#check for outliers
+#check for duplicates
+#chec
+
